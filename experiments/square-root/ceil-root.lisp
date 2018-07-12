9ad30fb958d2c2cf0b357b3551a4d1093a5a1a98
nadezhin <Dmitry.Nadezhin@gmail.com>
2018-07-13 00:33:21 +0300

    ceil-root takes real argument instead of natural

--- experiments/square-root/ceil-root.lisp
+++ experiments/square-root/ceil-root.lisp
@@ -7,11 +7,11 @@
 
 (with-arith5-help
  (define ceil-root-aux
-   ((x natp)
+   ((x real/rationalp)
     (y natp))
-   :measure (nfix (- x (nfix y)))
+   :measure (nfix (- (ceiling x 1) (nfix y)))
    :returns (result natp)
-   (b* ((x (nfix x))
+   (b* ((x (realfix x))
         (y (nfix y))
         ((when (<= x (* y y))) y))
      (ceil-root-aux x (1+ y)))
@@ -19,30 +19,32 @@
    (fty::deffixequiv ceil-root-aux)
    (defrule ceil-root-aux-upper-bound
      (b* ((root (ceil-root-aux x y)))
-       (<= (nfix x) (* root root))))
+       (<= (realfix x) (* root root))))
    (defrule ceil-root-aux-lower-bound
      (b* ((root1 (- (ceil-root-aux x y) 1)))
-       (implies (and (posp x)
+       (implies (and (real/rationalp x)
                      (natp y)
                      (< (* y y) x))
                 (and (< (* root1 root1) x)))))))
 
 (define ceil-root
-  ((x natp))
+  ((x real/rationalp))
   :returns (result natp)
   (ceil-root-aux x 0)
   ///
   (fty::deffixequiv ceil-root)
   (defrule ceil-root-upper-bound
     (b* ((root (ceil-root x)))
-      (<= (nfix x) (* root root)))
+      (<= (realfix x) (* root root)))
     :use (:instance ceil-root-aux-upper-bound (y 0)))
   (defrule ceil-root-lower-bound
     (b* ((root1 (- (ceil-root x) 1)))
-      (implies (posp x)
+      (implies (and (real/rationalp x)
+                    (< 0 x))
                (< (* root1 root1) x)))
     :use (:instance ceil-root-aux-lower-bound (y 0)))
   (defrule ceil-root-default
-    (implies (not (posp x))
+    (implies (or (not (real/rationalp x))
+                 (<= x 0))
              (equal (ceil-root x) 0))
     :enable ceil-root-aux))
